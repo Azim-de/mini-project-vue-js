23a30179002522341682a0fc0c0dbc1eee129f66
Azim Ilhamdi <85323068+Azim-de@users.noreply.github.com>
2022-05-16 22:49:19 +0700

    Add files via upload

---
+++ Mini Project/src/components/HeaderPage.vue
@@ -0,0 +1,48 @@
+<template>
+   <div class="nav">
+       <router-link to="/">Home</router-link>
+       <router-link>Add</router-link>
+       <router-link>Update</router-link>
+       <router-link to="/data-person">Data</router-link>
+       <a v-on:click="logout" href="#">Logout</a>
+
+   </div>
+</template>
+
+<script>
+export default{
+    name:'HeaderPage',
+    methods:{
+        logout(){
+            localStorage.clear();
+            this.$router.push({name:'SignUp'})
+        }
+
+    }
+}
+</script>
+
+<style>
+.nav{
+    background-color: rgb(205, 25, 19);
+    overflow:hidden;
+
+}
+
+.nav a{
+    float:left;
+    color:white;
+    padding:14px 16px;
+    text-align: center;
+    font-size:17px;
+    text-decoration: none;
+    margin-right:5px;
+}
+
+.nav a:hover{
+    background:white;
+    color: #333;
+    border-radius: 2px;
+}
+
+</style>
